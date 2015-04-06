65052709e020a76740ebac7d5ab6da315b288b21
King-JXN <18215611551@163.com>
2015-04-07 00:31:55 +0800

    Picture

--- src/Blog/views.py
+++ src/Blog/views.py
@@ -3,6 +3,7 @@ from django.template import loader, Context
 # from django.template import loader, RequestContext
 from django.http import HttpResponse
 from Blog.models import BlogPost
+from django.template.context import RequestContext
 
 # Create your views here.
 
@@ -10,5 +11,6 @@ def archive(request):
     posts = BlogPost.objects.all()
     t = loader.get_template("archieve.html")
     c = Context({'posts':posts})
-    return HttpResponse(t.render(c))
+#     return HttpResponse(t.render(c))
+    return render_to_response("archieve.html", HttpResponse(t.render(c)))
 #     return HttpResponse("Hello, world.")
--- src/DjangoTest/urls.py
+++ src/DjangoTest/urls.py
@@ -1,11 +1,15 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from Blog.views import archive
+
+import settings
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'DjangoTest.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^blog/$', archive)
+    url(r'^blog/$', archive),
+    url(r'^static/(?P<path>.*)', 'django.views.static.serve',{'document_root': settings.STATIC_PATH}),
 )
